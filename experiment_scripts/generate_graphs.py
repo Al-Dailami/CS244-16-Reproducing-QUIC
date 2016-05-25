ad40c3a0f6662fbdeb92f07e3cb0a859d01e064c
Anna Saplitski <anna.saplitski@gmail.com>
2016-05-25 14:25:44 -0700

    Updated wget to be timed and python data extraction accordingly. Additionally, added mm-delay, and bandwidth looping.

--- experiment_scripts/generate_graphs.py
+++ experiment_scripts/generate_graphs.py
@@ -3,39 +3,11 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
-FILE_REGEX = "(?P<loss>0.[0-9]+).out"
+FILENAME_REGEX = "(?P<loss>0\.[0-9]+)\.out"
+SECS_REGEX = "(?P<min>[0-9]+):(?P<secs>[0-9][0-9].[0-9]+)"
+REGEX = ".*?%s.*?%s" % (FILENAME_REGEX, SECS_REGEX)
 
-TCP_UNITS_REGEX = "\((?P<rate>[0-9]+(?:\.[0-9]+)?) (?P<units>(?:M|K)B/s)\)"
-TCP_REGEX = ".*?%s:.*?%s" % (FILE_REGEX, TCP_UNITS_REGEX)
-
-QUIC_SECS_REGEX = "(?P<min>[0-9]+):(?P<secs>[0-9][0-9].[0-9]+)"
-QUIC_REGEX = ".*?%s.*?%s" % (FILE_REGEX, QUIC_SECS_REGEX)
-
-def extract_data(filename, protocol, regex):
-    assert protocol in filename
-
-    f = open(filename, 'r')
-
-    loss_to_tput = {}
-    for line in f:
-        match = re.search(regex, line)
-        if match:
-            print "%s:%s%s" % (match.group("loss"), match.group("rate"), match.group("units"))
-        else:
-            print line
-        loss_to_tput[float(match.group("loss"))] = convert_to_mbps(match.group("rate"), match.group("units"))
-    print loss_to_tput
-    return loss_to_tput
-
-def convert_to_mbps(rate, units):
-    if units == "MB/s":
-        return float(rate)
-    elif units == "KB/s":
-        return float(rate) / 1000
-    else:
-        assert false
-
-def extract_quic_data(filename, protocol, regex, file_size):
+def extract_data(filename, protocol, regex, file_size):
     assert protocol in filename
 
     f = open(filename, 'r')
@@ -54,8 +26,9 @@ def convert_secs_to_mbps(file_size, secs):
 def main():
     assert len(sys.argv) == 5
 
-    tcp_loss_to_tput = extract_data(sys.argv[1], "tcp", TCP_REGEX)
-    quic_loss_to_tput = extract_quic_data(sys.argv[2], "quic", QUIC_REGEX, int(sys.argv[3]))        
+    tcp_loss_to_tput = extract_data(sys.argv[1], "tcp", REGEX, int(sys.argv[3]))
+    quic_loss_to_tput = extract_data(sys.argv[2], "quic", REGEX, int(sys.argv[3]))
+
     plt.scatter(tcp_loss_to_tput.keys(), tcp_loss_to_tput.values(), color='red')
     plt.scatter(quic_loss_to_tput.keys(), quic_loss_to_tput.values(), color='green')
     plt.savefig(sys.argv[4] + ".png")
