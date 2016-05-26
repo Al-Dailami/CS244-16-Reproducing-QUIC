18edeac2f1d892e6c25f55f248f9ead74bf7fec7
Anna Saplitski <anna.saplitski@gmail.com>
2016-05-25 20:54:55 -0700

    Added delay loop.

--- experiment_scripts/generate_graphs.py
+++ experiment_scripts/generate_graphs.py
@@ -21,16 +21,22 @@ def extract_data(filename, protocol, regex, file_size):
     return loss_to_tput
 
 def convert_secs_to_mbps(file_size, secs):
-    return (file_size/secs)/1000000
+    return (file_size/secs) * 8/1000000
 
 def main():
-    assert len(sys.argv) == 5
+    assert len(sys.argv) == 6
 
     tcp_loss_to_tput = extract_data(sys.argv[1], "tcp", REGEX, int(sys.argv[3]))
     quic_loss_to_tput = extract_data(sys.argv[2], "quic", REGEX, int(sys.argv[3]))
 
-    plt.scatter(tcp_loss_to_tput.keys(), tcp_loss_to_tput.values(), color='red')
-    plt.scatter(quic_loss_to_tput.keys(), quic_loss_to_tput.values(), color='green')
-    plt.savefig(sys.argv[4] + ".png")
+    plt.title("%s Mbps Bandwidth with %sms Delay" % (sys.argv[4], sys.argv[5]))
+    plt.xlabel("Packet loss rate (%)")
+    plt.ylabel("Goodput (Mbps)")
+    plt.scatter(tcp_loss_to_tput.keys(), tcp_loss_to_tput.values(), color='red', label="TCP")
+    plt.scatter(quic_loss_to_tput.keys(), quic_loss_to_tput.values(), color='blue', label="QUIC")
+    plt.ylim(ymin=0)
+    plt.xlim(xmin=-0.01)
+    plt.legend()
+    plt.savefig(sys.argv[4] + "mb-" + sys.argv[5] + "ms.png")
 
 if __name__ == "__main__": main()
--- experiment_scripts/run_test.sh
+++ experiment_scripts/run_test.sh
@@ -13,10 +13,19 @@ pkill -f quic_server &> /dev/null
 # Part I: Set up the http server and run wget through mahimahi.
 
 # Tuneable parameters
-BANDWIDTHS=( 1 2 4 5 10 20 50 100 )
-LOSS_RATES=( 0.0 0.005 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.17 )
+DELAYS=( 1 5 10 )
+BANDWIDTHS=( 5 100 )
+LOSS_RATES=( 0.0 0.1 )
+#BANDWIDTHS=( 1 2 4 5 10 20 50 100 )
+#LOSS_RATES=( 0.0 0.005 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.17 )
 
 SCRIPTS_DIR=~/CS244-16-Reproducing-QUIC/experiment_scripts
+
+for DELAY in "${DELAYS[@]}"
+do
+
+echo "Running with delay $DELAY"
+
 for BANDWIDTH in "${BANDWIDTHS[@]}"
 do 
     echo "Running with bandwidth $BANDWIDTH"
@@ -28,15 +37,15 @@ do
     HOST_IP=$(echo `hostname -I`)
 
     TRACE_DIR=~/CS244-16-Reproducing-QUIC/traces
-    MM_CMD="mm-link $TRACE_DIR/${BANDWIDTH}mb.trace $TRACE_DIR/${BANDWIDTH}mb.trace mm-delay 10 mm-loss downlink"
+    MM_CMD="mm-link $TRACE_DIR/${BANDWIDTH}mb.trace $TRACE_DIR/${BANDWIDTH}mb.trace mm-delay $DELAY mm-loss downlink"
 
     for LOSS in "${LOSS_RATES[@]}"
     do
 	echo "Running TCP client with loss rate $LOSS"
-	$MM_CMD $LOSS -- time -v -o $TMP_DIR/tcp_${BANDWIDTH}_${LOSS}.out wget http://${HOST_IP}:8000
+	$MM_CMD $LOSS -- time -v -o $TMP_DIR/tcp_${DELAY}_${BANDWIDTH}_${LOSS}.out wget http://${HOST_IP}:8000
     done
 
-    grep -i "elapsed" $TMP_DIR/tcp_${BANDWIDTH}* > $TMP_DIR/tcp_loss_${BANDWIDTH}_aggregate.txt
+    grep -i "elapsed" $TMP_DIR/tcp_${DELAY}_${BANDWIDTH}* > $TMP_DIR/tcp_loss_${DELAY}_${BANDWIDTH}_aggregate.txt
 
     # Clean up
     rm index.html.* &> /dev/null
@@ -55,16 +64,17 @@ do
     for LOSS in "${LOSS_RATES[@]}"
     do
 	echo "Running QUIC client with loss rate $LOSS"
-	$MM_CMD $LOSS -- time -v -o $TMP_DIR/quic_${BANDWIDTH}_${LOSS}.out ./out/Debug/quic_client  --host=100.64.0.1 --port=6121 https://www.example.org/index.html &> $TMP_DIR/quic_OUTPUT_${BANDWIDTH}_${LOSS}.out
+	$MM_CMD $LOSS -- time -v -o $TMP_DIR/quic_${DELAY}_${BANDWIDTH}_${LOSS}.out ./out/Debug/quic_client  --host=100.64.0.1 --port=6121 https://www.example.org/index.html &> $TMP_DIR/quic_OUTPUT_${DELAY}_${BANDWIDTH}_${LOSS}.out
     done
 
-    grep -i "elapsed" $TMP_DIR/quic_${BANDWIDTH}* > $TMP_DIR/quic_loss_${BANDWIDTH}_aggregate.txt
+    grep -i "elapsed" $TMP_DIR/quic_${DELAY}_${BANDWIDTH}* > $TMP_DIR/quic_loss_${DELAY}_${BANDWIDTH}_aggregate.txt
 
     pkill -f quic_server &> /dev/null
 
     cd $SCRIPTS_DIR
     FILE_SIZE=`wc -c ~/chromium/quic-data/www.example.org/index.html  | head -n1 | cut -d " " -f1`
-    python generate_graphs.py $TMP_DIR/tcp_loss_${BANDWIDTH}_aggregate.txt $TMP_DIR/quic_loss_${BANDWIDTH}_aggregate.txt $FILE_SIZE $BANDWIDTH
+    python generate_graphs.py $TMP_DIR/tcp_loss_${DELAY}_${BANDWIDTH}_aggregate.txt $TMP_DIR/quic_loss_${DELAY}_${BANDWIDTH}_aggregate.txt $FILE_SIZE $BANDWIDTH $DELAY
 
     #rm -rf $TMP_DIR/*
+done
 done
